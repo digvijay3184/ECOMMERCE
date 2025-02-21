9f46938602edcb1d099e31b5fafb6f884a7f6f01
Digvijay Singh <113190813+digvijay3184@users.noreply.github.com>
2025-02-21 11:50:35 +0530

    working on the filter option

--- server/controllers/shopping/productscontroller.js
+++ server/controllers/shopping/productscontroller.js
@@ -1,23 +1,52 @@
 const Product = require('../../models/Product')
 
 
-const getFilterProducts = async(req,res)=>{
-    try{
-        const products = await Product.find({});
+const getFilterProducts = async (req, res) => {
+    try {
+    const { category = [], brand = [], sortBy = 'price-lowtohigh' } = req.query;
 
-        res.status(200).json({
-            success: true ,
-            data: products,
-        })
-
-    }catch(e){
-        console.log(e);
-        res.status(500).json({
-            success : false ,
-            message:"some error occured"
-        })
+    let filter = {};
+    if (category.length > 0) {
+    filter.category = { $in: category.split(',') };
     }
-}
+    if (brand.length > 0) {
+    filter.brand = { $in: brand.split(',') };
+    }
+  
+      let sort = {};
+      switch (sortBy) {
+        case 'price-lowtohigh':
+          sort = { price: 1 };
+          break;
+        case 'price-hightolow':
+          sort = { price: -1 };
+          break;
+        case 'title-atoz':
+          sort = { title: 1 };
+          break;
+        case 'title-ztoa':
+          sort = { title: -1 };
+          break;
+        default:
+          sort = { price: 1 };
+          break;
+      }
+  
+      const products = await Product.find(filter).sort(sort);
+  
+      res.status(200).json({
+        success: true,
+        data: products,
+      });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({
+        success: false,
+        message: "An error occurred while fetching products",
+        error: e.message,
+      });
+    }
+  };
 
 
 module.exports = {getFilterProducts}
